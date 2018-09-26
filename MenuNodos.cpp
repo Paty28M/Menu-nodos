8b9ebe060547c838bb2c6f437ce5af759fcf3116
Sandra Patricia Martinez <patricia.sandra.martinez@gmail.com>
2018-09-26 05:12:00 -0500

    first commit

---
+++ MenuNodos.cpp
@@ -0,0 +1,132 @@
+include <iostream>
+#include <conio.h>
+using namespace std;
+struct Nodo{
+	int dato;
+	struct Nodo *siguiente;
+}*inicio,*aux,*actual,*anterior;
+
+void insertarNodo(int);
+void eliminarNodo(int);
+void mostrarNodos();
+void ordenarNodos();
+int main(int argc, char** argv) {
+	inicio=NULL;
+	int opcion=1;
+	int valor=0;
+	int cantidad;
+	int numero1;
+	cout<<"Creado por: Sandra Patricia Martinez Legorreta"<<endl;
+	cout<<"ICO-22"<<endl;
+	cout<<"ESTRUCUTURAS DE DATOS"<<endl<<endl;
+	do{
+		cout<<"\n Menu "<<endl;
+		cout<<"1.- INSERTAR NODO"<<endl;
+		cout<<"2.- ELIMINAR NODO"<<endl;
+		cout<<"3.- MOSTRAR NODOS"<<endl;
+		cout<<"4.- ORDENAR NODOS"<<endl;
+		cout<<"5.- Salir"<<endl;
+		cin>>opcion;
+		if(opcion==1){
+	cout<<"¿Cuantos datos quieres ingresar? ";
+	cin>>cantidad;
+	for(int i=1; i<=cantidad; i++){
+		cout<<"Ingresa el primer dato: ";
+		cin>>numero1;
+		insertarNodo(numero1);
+	}
+			insertarNodo(valor);
+		}else if(opcion==2){
+			cout<<"Valor del nodo a borrar: ";
+			cin>>valor;
+			eliminarNodo(valor);
+		}else if(opcion==3){
+			mostrarNodos();
+		}else if(opcion==4){
+			ordenarNodos();
+		}else{	
+			cout<<"terminado\n\n";
+		}
+		
+	}while(opcion!=5);
+
+	mostrarNodos();
+		
+	cin.get();
+	return 0;
+}
+void insertarNodo(int valor){
+	actual=inicio;
+	if(actual==NULL){
+			aux=new Nodo();
+			aux->dato=valor;
+			aux->siguiente=NULL;
+			inicio=aux;
+	}else{
+		while(actual->siguiente!=NULL)
+		{
+			actual=actual->siguiente;
+			}		
+		aux=new Nodo();
+		aux->dato=valor;
+		aux->siguiente=NULL;
+		actual->siguiente=aux;
+	}
+}
+
+void eliminarNodo(int valor){
+	if(inicio!=NULL){
+		aux=NULL;
+		actual=inicio;
+		while((actual!=NULL) && (actual->dato!=valor)){
+			aux=actual;
+			actual=actual->siguiente;
+		}
+		if(actual==NULL){
+			cout<<"no encontrado"<<endl;
+		}else if(aux==NULL){
+			inicio=inicio->siguiente;
+			delete actual;
+		}else{
+			aux->siguiente=actual->siguiente;
+			delete actual;
+		}
+	}
+}
+void mostrarNodos(){
+	actual=inicio;
+	cout<<" \nMostrar\n ";
+	while(actual!=NULL){
+		cout<<" -> "<<actual->dato<<" ";
+		actual=actual->siguiente;
+	}
+	cout<<"\n";
+}
+void ordenarNodos(){
+	if(inicio!=NULL){		
+		anterior=NULL;
+		aux=NULL;
+		actual=inicio;
+		while((actual->siguiente!=NULL) && (actual->dato < 
+actual->siguiente->dato)){							
+				anterior=actual;
+				actual=actual->siguiente;
+		}
+			if (actual->siguiente==NULL)
+			{
+				cout<<"\nordenamiento terminado\n";
+			}else if(anterior==NULL){
+				aux=actual;
+				actual=actual->siguiente;
+				inicio=actual;
+				aux->siguiente=actual->siguiente;
+				actual->siguiente=aux;
+			}else{
+				aux=actual;
+				actual=actual->siguiente;
+				anterior->siguiente=actual;
+				aux->siguiente=actual->siguiente;
+				actual->siguiente=aux;
+			}
+	}
+}
